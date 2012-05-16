57aab4c8bf09ce99147e6c22de01cd1fad90c1dc
jatsz <Jerry.Chou@Tobii.com>
2012-05-16 17:39:25 +0800

    write a simple web proxy using tornado

---
+++ python/online-web-proxy/online-web-proxy.py
@@ -0,0 +1,24 @@
+import urllib2
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+        
+    def post(self):
+        req = self.request
+        url = req.arguments["input-url"][0]
+        f = urllib2.urlopen(url)
+        self.write(f.read())
+        
+        
+application = tornado.web.Application([
+  (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(8001)
+    tornado.ioloop.IOLoop.instance().start()
